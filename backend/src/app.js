82d40d657d9e5faf192c044e4ede107b94f13bd7
Ayan-source <ayananwer43@gmail.com>
2026-07-09 03:20:01 +0500

    added schema

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,23 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello, Worldjfkljsdkljf!');
+});
+
+app.post('/admin',(req,res) => {
+    const {user,password} = req.body;
+    res.status(201).json(
+        {
+            message:"hello from post123",
+            user,password
+        }
+    )
+})
+
+app.get('/admin',(req,res)=>{
+    res.send("hello from get")
+})
+
+module.exports = app;
